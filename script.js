3842eb6a0e0670818746b613d5a29b821ccd6b98
Muhammad Daffa <111522987+mdafftfa@users.noreply.github.com>
2023-01-19 13:19:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const sectionAll = document.querySelectorAll('section[id]');
+window.addEventListener('scroll',()=>{
+	const scrollY = window.pageYOffset;
+	sectionAll.forEach((current)=>{
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 200;
+		const sectionId = current.getAttribute('id');
+		console.log(sectionId);
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			document
+				.querySelector('li a[href*="' + sectionId + '"]')
+				.classList.add('active');
+		}else{
+			document
+				.querySelector('li a[href*="' + sectionId + '"]')
+				.classList.remove('active');
+		}
+	});
+});
